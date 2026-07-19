3d174b3d121199e303e5dd6ee8310d1fb5c5e4bc
Thomas Park <thomaspark2005@gmail.com>
2026-07-19 18:34:02 -0400

    Wait classes in Python actually work?

--- test.py
+++ test.py
@@ -8,49 +8,80 @@ Bouncing ball test for pygame
 @author: thomaspark
 """
 
-import pygame, sys, random
+import pygame, sys
 
-pygame.init()
+width, height = 720, 540
 
-pygame.display.set_caption("Deltarune Chapter 6")
-
-gameIcon = pygame.image.load("testdata/icon.png")
-pygame.display.set_icon(gameIcon)
-
-size = width, height = 720, 540
-
-speed = [4,4]
-color = 67, 28, 150
-
-clock = pygame.time.Clock()
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("testdata/ralsei.png")
-ballrect = ball.get_rect()
-
-gameIsOn = True
-
-while gameIsOn:
-    events = pygame.event.get()
-    for e in events:
-        if e.type == pygame.QUIT:
-            gameIsOn = False
+class RalseiObj(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        
+        """Load Ralsei's sprite and set the Rect correctly"""
+        self.image = pygame.image.load("testdata/ralsei.png")
+        self.rect = self.image.get_rect()
+        
+    def update(self, speed):
+        
+        """Move Ralsei"""
+        self.rect = self.rect.move(speed)
+        
+        """Check for edge of screen with Ralsei's rect"""
+        if self.rect.left < 0 or self.rect.right > width:
+            speed[0] = -speed[0]
+        if self.rect.top < 0 or self.rect.bottom > height:
+            speed[1] = -speed[1]
             
-    ballrect = ballrect.move(speed)
+        
+def main():
+
+    """Initalizing everything"""
+    pygame.init()
     
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height - 10:
-        speed[1] = -speed[1]
-            
-    screen.fill(color)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    """Window settings"""
+    pygame.display.set_caption("Deltarune Chapter 6")
     
-    clock.tick(60)
-            
-print("Zamn")
- 
+    gameIcon = pygame.image.load("testdata/icon.png")
+    pygame.display.set_icon(gameIcon)
+    
+    """Size of screen"""
+    size = width, height
+    
+    """Speed of Ralsei"""
+    speed = [4,4]
+    
+    BGcolor = 67, 28, 150
+    
+    clock = pygame.time.Clock()
+    
+    screen = pygame.display.set_mode(size)
+    
+    """Create Ralsei sprite"""
+    sprites = pygame.sprite.Group(RalseiObj())
+    
+    
+    """Game loop"""
+    gameIsOn = True
+    
+    while gameIsOn:
+        
+        """Check if application is closed, close game if so"""
+        events = pygame.event.get()
+        for e in events:
+            if e.type == pygame.QUIT:
+                gameIsOn = False
+         
+        """Move Ralsei"""
+        sprites.update(speed)
+                
+        """Update screen"""
+        screen.fill(BGcolor)
+        sprites.draw(screen)
+        pygame.display.flip()
+        
+        clock.tick(60)
+                
+    print("Done")
+
+main() 
 pygame.quit()
 sys.exit()
