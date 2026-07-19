beb69a11605a341b6a3fc0e6b2e952c4c02442d5
Thomas Park <thomaspark2005@gmail.com>
2026-07-19 16:31:38 -0400

    Added Ralsei

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ Bouncing ball test for pygame
 @author: thomaspark
 """
 
-import pygame, sys
+import pygame, sys, random
 
 pygame.init()
 
@@ -18,31 +18,33 @@ gameIcon = pygame.image.load("testdata/icon.png")
 pygame.display.set_icon(gameIcon)
 
 size = width, height = 720, 540
-speed = [2,2]
-black = 0, 0, 0
+
+speed = [4,4]
+color = 67, 28, 150
 
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("testdata/icon.png")
+ball = pygame.image.load("testdata/ralsei.png")
 ballrect = ball.get_rect()
 
 gameIsOn = True
 
 while gameIsOn:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    events = pygame.event.get()
+    for e in events:
+        if e.type == pygame.QUIT:
             gameIsOn = False
             
     ballrect = ballrect.move(speed)
     
     if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-    
-    screen.fill(black)
+        speed[0] = -random.randint(1, 10)
+    if ballrect.top < 0 or ballrect.bottom > height - 10:
+        speed[1] = -random.randint(1, 10)
+            
+    screen.fill(color)
     screen.blit(ball, ballrect)
     pygame.display.flip()
     
